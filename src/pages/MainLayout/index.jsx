fb67a2ca6939098199af41292366e28355b30245
Lendulka <cernohorska.lc@gmail.com>
2024-04-17 22:55:31 +0200

    HOTOVO

--- src/pages/MainLayout/index.jsx
+++ src/pages/MainLayout/index.jsx
@@ -1,7 +1,22 @@
 import { Link, Outlet } from 'react-router-dom'
+import ScrollToTop from 'react-scroll-up'
+import IconUp from './img/icon.png'
 import './style.css'
 
 function MainLayout() {
+
+    const scrollStyle = {
+        position: "sticky",
+        bottom: "0px",
+        right: "0px",
+        display: "flex",
+        justifyContent: "end",
+    }
+
+    const iconStyle = {
+        opacity: 0.6,
+    }
+
     return (
         <div className="container">
 
@@ -13,6 +28,10 @@ function MainLayout() {
 
             <main className="main">
                 <Outlet />
+
+                <ScrollToTop showUnder={160} style={scrollStyle}>
+                    <span><img src={IconUp} alt="Icon-Up" style={iconStyle} /></span>
+                </ScrollToTop>
             </main>
 
             <footer className="footer">
